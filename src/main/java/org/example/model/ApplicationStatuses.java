593135816327ada8afe008bd637bc1650627ce6e
Alexis1007 <alexis.1007@hotmail.com>
2025-03-13 12:09:35 -0600

    add entity classes for application statuses, loan types, and user types, ERD updated

---
+++ src/main/java/org/example/model/ApplicationStatuses.java
@@ -0,0 +1,48 @@
+package org.example.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "application_statuses")
+public class ApplicationStatuses{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "application_statuses_id")
+    private int id;
+
+    @Column(name = "application_statuses", length = 10, unique = true)
+    private String status;
+
+    @Column(name = "description", length = 100)
+    private String description;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+}
