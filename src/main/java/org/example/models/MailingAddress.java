cb4068377342a7a513db4dad9d7377aa811f3978
Jorge Medina <medinasoad@hotmail.com>
2025-03-13 11:19:44 -0600

    Adding mailing address entity

---
+++ src/main/java/org/example/models/MailingAddress.java
@@ -0,0 +1,77 @@
+package org.example.models;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "mailing_addresses")
+public class MailingAddress {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = true, length = 30)
+    private String street;
+
+    @Column(nullable = true, length = 30)
+    private String city;
+
+    @Column(nullable = true, length = 30)
+    private String state;
+
+    @Column(nullable = true, length = 30)
+    private String zip;
+
+    @Column(nullable = true, length = 30)
+    private String country;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
