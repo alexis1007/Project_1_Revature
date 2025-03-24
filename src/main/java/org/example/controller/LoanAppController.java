2cb17a4e188cac64b3cf4f3b6cec2d17fd2eabd8
Pablo Bravo <pablo1221bravo@gmail.com>
2025-03-24 15:24:35 -0600

    Loggs in authcontroller, loanappcontroller, userprofilecontroller

--- src/main/java/org/example/controller/LoanAppController.java
+++ src/main/java/org/example/controller/LoanAppController.java
@@ -2,11 +2,14 @@ package org.example.controller;
 
 import java.util.List;
 
+import lombok.extern.slf4j.Slf4j;
 import org.example.DTO.LoanResponseDto;
 import org.example.Service.LoanApplicationService;
 import org.example.Service.MotivationalQuoteService;
 import org.example.model.LoanApplication;
 import org.example.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -20,10 +23,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
-
+@Slf4j
 @RestController
 @RequestMapping("/api/loans")
 public class LoanAppController {
+    private static final Logger log = LoggerFactory.getLogger(LoanAppController.class);
     private final LoanApplicationService loanAppService;
     private final MotivationalQuoteService motivationalQuoteService;
 
@@ -37,6 +41,7 @@ public class LoanAppController {
      */
     @GetMapping
     public ResponseEntity<List<LoanApplication>> getAllLoans() {
+        log.info("Request all loans");
         return ResponseEntity.ok(loanAppService.findAllLoans());
     }
 
@@ -45,6 +50,7 @@ public class LoanAppController {
      */
     @GetMapping("/{id}")
     public ResponseEntity<LoanApplication> getLoanById(@PathVariable Long id) {
+        log.info("Request loan with id [{}]",id);
         return loanAppService.findLoanById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
@@ -62,12 +68,15 @@ public class LoanAppController {
     @PostMapping
     public ResponseEntity<?> createLoan(@RequestBody LoanApplication loan, HttpServletRequest request) {
         User sessionUser = (User) request.getAttribute("user");
+        log.info("User [{}] requests to create a new loan", sessionUser.getId());
         if (sessionUser == null) {
+            log.warn("Access denied to user [{}]",sessionUser.getId());
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Not logged in");
         }
-        
+
         loan.setUserProfile(sessionUser.getUserProfile());
         LoanApplication savedLoan = loanAppService.createLoan(loan);
+        log.info("Loan created by user [{}]",sessionUser.getId());
         String quote = motivationalQuoteService.getRandomQuote();
         LoanResponseDto responseDto = new LoanResponseDto(savedLoan, quote);
         return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
@@ -79,6 +88,7 @@ public class LoanAppController {
     @PutMapping("/{id}")
     public ResponseEntity<LoanApplication> updateLoan(@PathVariable Long id,
             @RequestBody LoanApplication loanApplication) {
+        log.info("Updating loan with id [{}]",id);
         return loanAppService.updateLoan(id, loanApplication)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
@@ -90,12 +100,14 @@ public class LoanAppController {
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteLoan(@PathVariable Long id) {
         boolean deleted = loanAppService.deleteLoan(id);
+        log.info("Deleteing loan with id [{}]",id);
         return deleted ? ResponseEntity.noContent().build() : ResponseEntity.notFound().build();
     }
 
     @PutMapping("/{id}/approve")
     public ResponseEntity<LoanApplication> approveLoan(@PathVariable Long id,
             @RequestBody LoanApplication loanApplication) {
+        log.info("Approving loan with id [{}]",id);
         return loanAppService.approveLoan(id, loanApplication)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
@@ -104,6 +116,7 @@ public class LoanAppController {
     @PutMapping("/{id}/reject")
     public ResponseEntity<LoanApplication> rejectLoan(@PathVariable Long id,
             @RequestBody LoanApplication loanApplication) {
+        log.info("Rejecting loan with id [{}]",id);
         return loanAppService.rejectLoan(id, loanApplication)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
